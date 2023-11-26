fea1394c84878c63be85897f17539631d747ee5c
kolbe <water.northground@gmail.com>
2023-11-26 10:45:52 +0900

    outline for game page

---
+++ lib/widget/pages/game_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class GamePage extends StatefulWidget {
+  const GamePage({super.key});
+
+  @override
+  State<GamePage> createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
