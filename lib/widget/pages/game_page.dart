d4c4d5f98cebfdda4aec72198a2e6a4679ff85e3
kolbe <water.northground@gmail.com>
2023-12-11 23:31:01 +0900

    game page

--- lib/widget/pages/game_page.dart
+++ lib/widget/pages/game_page.dart
@@ -1,24 +1,15 @@
-import 'package:flame/game.dart';
-import 'package:flame_realtime_shooting/game/game_engine.dart';
-import 'package:flame_realtime_shooting/widget/components/lobby_dialog.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-
 class GamePage extends StatefulWidget {
-  const GamePage({super.key});
+  const GamePage({Key? key}) : super(key: key);
 
   @override
   State<GamePage> createState() => _GamePageState();
 }
 
 class _GamePageState extends State<GamePage> {
-  late final GameEngine _gameEngine;
+  late final MyGame _game;
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-  }
+  /// Holds the RealtimeChannel to sync game states
+  RealtimeChannel? _gameChannel;
 
   @override
   Widget build(BuildContext context) {
@@ -27,32 +18,99 @@ class _GamePageState extends State<GamePage> {
         fit: StackFit.expand,
         children: [
           Image.asset('assets/images/background.jpg', fit: BoxFit.cover),
-          GameWidget(game: _gameEngine),
+          GameWidget(game: _game),
         ],
       ),
     );
   }
 
+  @override
+  void initState() {
+    super.initState();
+    _initialize();
+  }
+
   Future<void> _initialize() async {
-    _gameEngine = GameEngine(
-      onGameOver: (bool) {},
-      onGameStateUpdate: (Vector2, int) {},
+    _game = MyGame(
+      onGameStateUpdate: (position, health) async {
+        ChannelResponse response;
+        // Loop until the send succeeds if the payload is to notify defeat.
+        do {
+          response = await _gameChannel!.send(
+            type: RealtimeListenTypes.broadcast,
+            event: 'game_state',
+            payload: {'x': position.x, 'y': position.y, 'health': health},
+          );
+
+          // wait for a frame to avoid infinite rate limiting loops
+          await Future.delayed(Duration.zero);
+          setState(() {});
+        } while (response == ChannelResponse.rateLimited && health <= 0);
+      },
+      onGameOver: (playerWon) async {
+        await showDialog(
+          barrierDismissible: false,
+          context: context,
+          builder: ((context) {
+            return AlertDialog(
+              title: Text(playerWon ? 'You Won!' : 'You lost...'),
+              actions: [
+                TextButton(
+                  onPressed: () async {
+                    Navigator.of(context).pop();
+                    await supabase.removeChannel(_gameChannel!);
+                    _openLobbyDialog();
+                  },
+                  child: const Text('Back to Lobby'),
+                ),
+              ],
+            );
+          }),
+        );
+      },
     );
 
-    // Widgetがマウントするのを待つために１フレームawaitする
+    // await for a frame so that the widget mounts
     await Future.delayed(Duration.zero);
-    if (mounted) {}
+
+    if (mounted) {
+      _openLobbyDialog();
+    }
   }
 
   void _openLobbyDialog() {
     showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (context) {
-        return LobbyDialog(
-          onGameStarted: (String) {},
-        );
-      },
-    );
+        context: context,
+        barrierDismissible: false,
+        builder: (context) {
+          return _LobbyDialog(
+            onGameStarted: (gameId) async {
+              // await a frame to allow subscribing to a new channel in a realtime callback
+              await Future.delayed(Duration.zero);
+
+              setState(() {});
+
+              _game.startNewGame();
+
+              _gameChannel = supabase.channel(gameId, opts: const RealtimeChannelConfig(ack: true));
+
+              _gameChannel!.on(RealtimeListenTypes.broadcast, ChannelFilter(event: 'game_state'), (payload, [_]) {
+                final position = Vector2(payload['x'] as double, payload['y'] as double);
+                final opponentHealth = payload['health'] as int;
+                _game.updateOpponent(
+                  position: position,
+                  health: opponentHealth,
+                );
+
+                if (opponentHealth <= 0) {
+                  if (!_game.isGameOver) {
+                    _game.isGameOver = true;
+                    _game.onGameOver(true);
+                  }
+                }
+              }).subscribe();
+            },
+          );
+        });
   }
 }
